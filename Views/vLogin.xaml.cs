6ae1e3913a756910c4c4c3368ce210ab42d8952b
Jazmin <e1725897548@uisrael.edu.ec>
2025-10-29 20:47:36 -0500

    final

--- Views/vLogin.xaml.cs
+++ Views/vLogin.xaml.cs
@@ -2,8 +2,49 @@ namespace oherreraEXAMEN.Views;
 
 public partial class vLogin : ContentPage
 {
-	public vLogin()
+
+    private readonly Dictionary<string, string> usuariosValidos = new()
+    {
+        {"estudiante","moviles" },
+        {"uisrael","2025" }
+    };
+    public vLogin()
 	{
 		InitializeComponent();
 	}
+
+    private async void btnIngresar_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            string usuario = txtUsuario.Text?.Trim();
+            string contrasena = txtContrasena.Text?.Trim();
+
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contrasena))
+            {
+                DisplayAlert("Error", "Debe ingresar usuario y contraseña", "OK");
+                return;
+            }
+
+
+            if (usuariosValidos.TryGetValue(usuario, out string passwordCorrecto) && passwordCorrecto == contrasena)
+            {
+
+                Navigation.PushAsync(new vResgistro(usuario, contrasena));
+
+            }
+            else
+            {
+                await DisplayAlert("Error", "Usuario o contraseña incorrectos", "OK");
+
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error en login: {ex.Message}");
+
+        }
+
+    }
 }
--- Views/vResgistro.xaml.cs
+++ Views/vResgistro.xaml.cs
@@ -1,9 +1,80 @@
-namespace oherreraEXAMEN.Views;
-
-public partial class vResgistro : ContentPage
+﻿namespace oherreraEXAMEN.Views
 {
-	public vResgistro()
-	{
-		InitializeComponent();
-	}
+    public partial class vResgistro : ContentPage
+    {
+
+        //VARIABLES GLOBALES
+        string usuarioGlobal;
+        string contrasenaGlobal;
+
+
+
+        private const double costoCurso = 1500.0;
+
+        public vResgistro()
+        {
+            InitializeComponent();
+        }
+
+        public vResgistro(string usuario, string contrasena)
+        {
+            InitializeComponent();
+            usuarioGlobal = usuario;
+            contrasenaGlobal = contrasena;
+
+            lblSaludo.Text = $"Bienvenido {usuario}";
+
+
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (string.IsNullOrWhiteSpace(txtPagoInicial.Text))
+                {
+                    DisplayAlert("Error", "Ingrese el valor de inscripción.", "OK");
+                    return;
+                }
+
+                double pagoInicial = double.Parse(txtPagoInicial.Text);
+
+                if (pagoInicial >= costoCurso)
+                {
+                    DisplayAlert("Aviso", "El pago inicial no puede ser igual o mayor al costo del curso.", "OK");
+                    return;
+                }
+
+ 
+                double saldoRestante = costoCurso - pagoInicial;
+
+             
+                double cuotaBase = saldoRestante / 4;
+
+                double cuotaConInteres = cuotaBase + (cuotaBase * 0.04);
+
+                
+                double totalAPagar = (cuotaConInteres * 4) + pagoInicial;
+
+              
+                lblResultado.Text = $"Resultado:\n\n" +
+                    $"Costo del curso: ${costoCurso:F2}\n" +
+                    $"Pago inicial: ${pagoInicial:F2}\n" +
+                    $"Saldo restante: ${saldoRestante:F2}\n" +
+                    $"Cuota mensual (4% interés): ${cuotaConInteres:F2}\n" +
+                    $"Total final (incluyendo inscripción): ${totalAPagar:F2}";
+            }
+            catch
+            {
+                DisplayAlert("Error", "Ingrese valores numéricos válidos.", "OK");
+            }
+        }
+
+        private void btnEnviar_Clicked(object sender, EventArgs e)
+        {
+
+
+        }
+    }
 }
